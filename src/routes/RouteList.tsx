ec90212cd3ee8eada94fcc2bed9460824529d924
harry-oh.pnpt <101689392+harry-ohh@users.noreply.github.com>
2022-10-13 13:45:44 +0900

    react-query 캐싱 공부

--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
-import RQSuperHeros from "../components/RQSpuerHeros";
-import SuperHeros from "../components/SuperHeros";
+import RQSuperHeroes from "../components/RQSpuerHeroes";
+import SuperHeroes from "../components/SuperHeroes";
 
 const RouteList = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/super-heros" element={<SuperHeros />} />
-      <Route path="/rqSuper-heros" element={<RQSuperHeros />} />
+      <Route path="/super-heros" element={<SuperHeroes />} />
+      <Route path="/rqSuper-heros" element={<RQSuperHeroes />} />
     </Routes>
   );
 };
